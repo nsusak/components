07593455572118f623f33fc8de92d64c713e0cd1
nsusak <nikolina@travelraysoftware.com>
2023-11-07 10:02:57 +0100

    added sortable table comp

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AccordionPage from "./pages/AccordionPage";
 import DropdownPage from "./pages/DropdownPage";
 import ButtonPage from "./pages/ButtonPage";
 import ModalPage from "./pages/ModalPage";
+import TablePage from "./pages/TablePage";
 import SideBar from "./components/SideBar";
 function App() {
   return (
@@ -20,6 +21,9 @@ function App() {
         </Route>
         <Route path="/modal">
           <ModalPage />
+        </Route>
+        <Route path="/table">
+          <TablePage />
         </Route>
       </div>
     </div>
